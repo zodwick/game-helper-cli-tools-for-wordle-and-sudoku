e8adddf8397a50b1c7e4da3b633001fdb0352e35
zodwick <115868723+zodwick@users.noreply.github.com>
2023-03-30 01:29:49 +0530

    v2 start

---
+++ test.py
@@ -0,0 +1,24 @@
+# import pandas as pd
+# df = pd.read_csv("unigram_freq.csv")
+# matrix2 = df[df.columns[0]].as_matrix()
+# list2 = matrix2.tolist()
+# print(list2[0])
+
+import csv
+
+with open("unigram_freq.csv") as f:
+    list2 = [row.split(",")[0] for row in f]
+    list_5= list(i for i in list2 if len(i)==5)
+    # print(list_5)
+    
+    
+filename = "five_letter_worrrds.csv"
+    
+# writing to csv file 
+with open(filename, 'w') as csvfile: 
+    # creating a csv writer object 
+    csvwriter = csv.writer(csvfile) 
+     
+        
+    # writing the data rows 
+    csvwriter.writerows(list(i for i in list_5))
